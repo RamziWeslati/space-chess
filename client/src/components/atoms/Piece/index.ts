56fb2016b649c92b7527095fbc521c1341d4b9d1
Ramzi Oueslati <ramzi.oueslati@ensi-uma.tn>
2022-03-16 00:25:59 +0100

    feat(client): add pieces atoms

---
+++ client/src/components/atoms/Piece/index.ts
@@ -0,0 +1,14 @@
+import { Bishop } from "./Bishop";
+import { King } from "./King";
+import { Knight } from "./Knight";
+import { Pawn } from "./Pawn";
+import { Queen } from "./Queen";
+import { Rook } from "./Rook";
+
+type PieceProps = {
+  color: "white" | "black";
+  rotate?: boolean;
+};
+
+export type { PieceProps };
+export { King, Queen, Rook, Bishop, Knight, Pawn };
